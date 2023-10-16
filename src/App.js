ccf7459b16611d50758f88af8d2bbe962af5e132
Shamil Roshan <shamilroshan390@gmail.com>
2023-10-17 02:37:37 +0530

    Design is Full

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [ task , addTask ] = useState( '' )
+  const [ lists , addToList ] = useState( [] )
 
   return (
 
@@ -10,13 +14,64 @@ function App() {
 
         <div id="input-box">
 
-          <input type="text" name="" id="" placeholder='Enter the task' />
+          <input value={ task } onChange={ ( event ) => { addTask( event.target.value ) } } type="text" name="" id="" placeholder='Enter the task' />
           <i class='bx bx-plus'></i>
 
         </div>
 
       </section>
-      <section id='three-categories' ></section>
+      <section id='three-categories' >
+
+        <div className="categories-1">
+
+          <section className="heading"><p>Dropped</p></section>
+          <section className="tasks">
+
+            <div className="task">
+
+              <input type="checkbox" name="" id="" />
+              <input type="text" name="" id="" readOnly />
+              <i class='bx bx-x'></i>
+
+            </div>
+
+          </section>
+
+        </div>
+        <div className="categories-2">
+
+          <section className="heading"><p>Proceeding</p></section>
+          <section className="tasks">
+
+            <div className="task">
+
+              <input type="checkbox" name="" id="" />
+              <input type="text" name="" id="" readOnly />
+              <i class='bx bx-x'></i>
+
+            </div>
+
+          </section>
+
+        </div>
+        <div className="categories-3">
+
+          <section className="heading"><p>Completed</p></section>
+          <section className="tasks">
+
+            <div className="task">
+
+              <input type="checkbox" name="" id="" />
+              <input type="text" name="" id="" readOnly />
+              <i class='bx bx-x'></i>
+
+            </div>
+
+          </section>
+
+        </div>
+
+      </section>
 
     </div>
 
