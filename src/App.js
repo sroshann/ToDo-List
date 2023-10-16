efe3e18eea0739c3a6f63c1196a272511f603d67
Shamil Roshan <shamilroshan390@gmail.com>
2023-10-17 00:09:14 +0530

    Input box is created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 
 function App() {
+
   return (
+
     <div className="App">
-      <h1>hi</h1>
+
+      <section id='input' >
+
+        <div id="input-box">
+
+          <input type="text" name="" id="" placeholder='Enter the task' />
+          <i class='bx bx-plus'></i>
+
+        </div>
+
+      </section>
+      <section id='three-categories' ></section>
+
     </div>
+
   );
+
 }
 
 export default App;
